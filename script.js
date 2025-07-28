089d49061fb7e40b89cffbaf081c9a722d0af104
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-07-28 21:47:55 +0200

    Updated Script.js

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ const c = 8
 
 }
 
+let sentence = "codespace is awesome"
+let sentence2 = "This is another sentence"
+let sentence3 = "This is also a sentence"
+
+sentence.toUpperCase()
+
+//Comparison Operators 
+
+5 == 5  //true
+5 === '5' //false
+5 > 6  //false 
+5 < 6 //true
+5 >= 5 //true 
+5 <= 6  //true 
+
+
+//logical operators 
+
+&& - AND 
+|| - OR 
+! - NOT 
+
+
+
+
 
